feb38e487efa03da77dca087f7c36171f50887e0
XxrrxX-Ubuntu-windows <xxrrxx3888@gmail.com>
2023-04-17 06:45:51 -0600

    Se actualiza js

--- encriptar.js
+++ encriptar.js
@@ -1,4 +1,30 @@
 function encriptar(){
 var TE=document.getElementById("txtE").value;
-alert(TE);
+let SplitTE = TE.split('');
+
+SplitTE.forEach(letra => {
+
+if (letra == "e"){
+console.log("\n	");
+}
+
+else if (letra == "i"){
+console.log("imes");
+}
+
+else if (letra == "a"){
+console.log("ai");
+}
+else if (letra == "o"){
+console.log("ober");
+}
+else if (letra == "u"){
+console.log("ufat");
+
+}else{
+console.log(letra);
+}
+
+});
+
 }
